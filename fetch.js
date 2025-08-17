c28a7e7a2d657a813287d9937b3009d658eb1393
superfood ‎ <rookiecookierc@gmail.com>
2025-08-16 20:18:30 -0500

    did dynamic and stuff

--- fetch.js
+++ fetch.js
@@ -1,36 +1,13 @@
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
-const penguins = [
-  'https://penguin-api-superfood.vercel.app/.images/cooked.jpg',
-  "https://penguin-api-superfood.vercel.app/.images/penggy.png",
-  "https://penguin-api-superfood.vercel.app/.images/skiii.png",
-  'https://penguin-api-superfood.vercel.app/.images/jp.jpeg',
-  "https://penguin-api-superfood.vercel.app/.images/skippy.png",
-  'https://penguin-api-superfood.vercel.app/.images/ido.jpg',
-  "https://penguin-api-superfood.vercel.app/.images/helios522.jpeg",
-  "https://penguin-api-superfood.vercel.app/.images/c.jpg",
-  "https://penguin-api-superfood.vercel.app/.images/2.jpg",
-  "https://penguin-api-superfood.vercel.app/.images/1.jpg",
-  "https://penguin-api-superfood.vercel.app/.images/3.webp",
-  "https://penguin-api-superfood.vercel.app/.images/4.jpg",
-  'https://penguin-api-superfood.vercel.app/.images/webpwebpwebpwebp.webp',
-  "https://penguin-api-superfood.vercel.app/.images/miniahhhbooogh.jpeg",
-  "https://penguin-api-superfood.vercel.app/.images/ok.jpeg",
-  'https://penguin-api-superfood.vercel.app/.images/avif.jpeg',
-  "https://penguin-api-superfood.vercel.app/.images/avif.png",
-  "https://penguin-api-superfood.vercel.app/.images/pev.png",
-  'https://penguin-api-superfood.vercel.app/.images/fish.jpg',
-  'https://penguin-api-superfood.vercel.app/.images/poop.jpg',
-  'https://penguin-api-superfood.vercel.app/.images/bloons.jpg',
-  'https://penguin-api-superfood.vercel.app/.images/no.webp',
-  'https://penguin-api-superfood.vercel.app/.images/other.jpg',
-  'https://penguin-api-superfood.vercel.app/.images/fee.jpg',
-  'https://penguin-api-superfood.vercel.app/.images/biga.jpg',
-  'https://penguin-api-superfood.vercel.app/.images/kool.jpg',
-  'https://penguin-api-superfood.vercel.app/.images/new.jpg'
-];
-
+const cdnbase = Array.from({ length: 28 }, (_, id) =>
+  `https://penguin-api.rookiecookierc.workers.dev/assets/${id}.jpg`
+);
+const cdn1 = Array.from({ length: 26}, (_, i) =>
+  `https://cdn-penguins.netlify.app/assets/${i}.jpg`
+);
+const penguins = [...cdnbase, ...cdn1];
 async function handleRequest(_request) {
   const randomUrl = penguins[Math.floor(Math.random() * penguins.length)];
   const data = { image: randomUrl };
