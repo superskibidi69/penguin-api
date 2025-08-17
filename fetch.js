5bde167ba03f9e5eb46b6c8c18ee7e11b7c272c9
superfood ‎ <rookiecookierc@gmail.com>
2025-08-16 20:50:35 -0500

    try catch

--- fetch.js
+++ fetch.js
@@ -9,21 +9,31 @@ const cdn1 = Array.from({ length: 26 }, (_, i) =>
 );
 const penguins = [...cdn0, ...cdn1];
 async function handleRequest(event) {
-  const cache = caches.default;
-  const randomUrl = penguins[Math.floor(Math.random() * penguins.length)];
-  const cacheKey = new Request(randomUrl);
-  let response = await cache.match(cacheKey);
-  if (!response) {
-    const data = { image: randomUrl };
-    response = new Response(JSON.stringify(data), {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,OPTIONS",
-        "Access-Control-Allow-Headers": "*",
-        "Cache-Control": "public, max-age=6528"
-      }
+  try {
+    const cache = caches.default;
+    const randomUrl = penguins[Math.floor(Math.random() * penguins.length)];
+    const cacheKey = new Request(`penguin:${randomUrl}`);
+    let response = await cache.match(cacheKey);
+
+    if (!response) {
+      const data = { image: randomUrl };
+      response = new Response(JSON.stringify(data), {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,OPTIONS",
+          "Access-Control-Allow-Headers": "*",
+          "Cache-Control": "public, max-age=2700"
+        }
+      });
+      event.waitUntil(cache.put(cacheKey, response.clone()));
+    }
+
+    return response;
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500
     });
-    event.waitUntil(cache.put(cacheKey, response.clone()));
   }
-return response;}
+}
