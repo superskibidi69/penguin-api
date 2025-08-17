211b5b282b8a31128539dd8c9dd576bda2cf3d11
superfood ‎ <rookiecookierc@gmail.com>
2025-08-16 20:39:18 -0500

    add caching

--- fetch.js
+++ fetch.js
@@ -1,23 +1,29 @@
 addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
+  event.respondWith(handleRequest(event));
 });
-const cdn0 = Array.from({ length: 28 }, (_, id) =>
+const cdn0 = Array.from({ length: 29 }, (_, id) =>
   `https://penguin-api-superfood.vercel.app/assets/${id}.jpg`
 );
-const cdn1 = Array.from({ length: 26}, (_, i) =>
+const cdn1 = Array.from({ length: 26 }, (_, i) =>
   `https://cdn-penguins.netlify.app/assets/${i}.jpg`
 );
 const penguins = [...cdn0, ...cdn1];
-async function handleRequest(_request) {
+async function handleRequest(event) {
+  const cache = caches.default;
   const randomUrl = penguins[Math.floor(Math.random() * penguins.length)];
-  const data = { image: randomUrl };
-
-  return new Response(JSON.stringify(data), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,OPTIONS",
-      "Access-Control-Allow-Headers": "*"
-    }
-  });
-}
+  const cacheKey = new Request(randomUrl);
+  let response = await cache.match(cacheKey);
+  if (!response) {
+    const data = { image: randomUrl };
+    response = new Response(JSON.stringify(data), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+        "Cache-Control": "public, max-age=6528"
+      }
+    });
+    event.waitUntil(cache.put(cacheKey, response.clone()));
+  }
+return response;}
