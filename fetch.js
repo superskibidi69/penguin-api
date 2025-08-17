8bf79c4ebdc5294e3b2c3ec4e9fb4588f6bfbe49
superfood ‎ <rookiecookierc@gmail.com>
2025-08-17 10:59:53 -0500

    feenished improve buggin

--- fetch.js
+++ fetch.js
@@ -9,7 +9,7 @@ const cdn_1 = Array.from({ length: 29 }, (_, i) =>
 );
 const penguins = [...cdn_0, ...cdn_1];
 const GLOBAL_CACHE_NAME = "penguin-cache";
-const CACHE_TTL = 6000;
+const CACHE_TTL = 6524;
 async function handleRequest(request, event) {
   if (request.method === "OPTIONS") {
     return new Response(null, {
@@ -25,43 +25,26 @@ async function handleRequest(request, event) {
     const randomUrl = penguins[Math.floor(Math.random() * penguins.length)];
     const cacheKey = new Request(randomUrl);
     let response = await cache.match(cacheKey);
-    let cacheStatus = "MISS";
+    const cacheStatus = response ? "HIT" : "MISS";
     if (response) {
       console.log("cache hit:", randomUrl);
-      cacheStatus = "HIT";
     } else {
       console.log("cache miss:", randomUrl);
       const data = { image: randomUrl, cache: "MISS" };
-      response = new Response(JSON.stringify(data), {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,OPTIONS",
-          "Access-Control-Allow-Headers": "*",
-          "Cache-Control": `public, max-age=${CACHE_TTL}`,
-          "CDN-Cache-Control": `max-age=${CACHE_TTL}`,
-          "X-Cache-Status": "MISS",
-          "X-Penguin-URL": randomUrl,
-        },
-      });
+      response = new Response(JSON.stringify(data));
       event.waitUntil(cache.put(cacheKey, response.clone()));
     }
-    if (cacheStatus === "HIT") {
-      const data = { image: randomUrl, cache: "HIT" };
-      response = new Response(JSON.stringify(data), {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,OPTIONS",
-          "Access-Control-Allow-Headers": "*",
-          "Cache-Control": `public, max-age=${CACHE_TTL}`,
-          "CDN-Cache-Control": `max-age=${CACHE_TTL}`,
-          "X-Cache-Status": "HIT",
-          "X-Penguin-URL": randomUrl,
-        },
-      });
-    }
-    return response;
+    const headers = new Headers(response.headers);
+    headers.set("Content-Type", "application/json");
+    headers.set("Access-Control-Allow-Origin", "*");
+    headers.set("Access-Control-Allow-Methods", "GET,OPTIONS");
+    headers.set("Access-Control-Allow-Headers", "*");
+    headers.set("Cache-Control", `public, max-age=${CACHE_TTL}`);
+    headers.set("CDN-Cache-Control", `max-age=${CACHE_TTL}`);
+    headers.set("X-Cache-Status", cacheStatus);
+    headers.set("X-Penguin-URL", randomUrl);
+    const body = JSON.stringify({ image: randomUrl, cache: cacheStatus });
+    return new Response(body, { headers });
   } catch (err) {
     return new Response(JSON.stringify({ error: err.message }), {
       headers: { 
